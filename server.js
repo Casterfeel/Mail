3a1116d8912874b8a6e0799ba94268c5f2122ede
Casterfeel <sanikovmaxim@mail.ru>
2024-11-15 17:35:32 +0300

    sec serv commit

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',   
